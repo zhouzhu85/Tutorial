ce346bb3a4e4c6d2df3e6546272851ae5994f92c
周柱 <zhouzhu@hopechina.com>
2020-10-19 13:45:10 +0800

    IU-2019.2 <Administrator@PC-201804121451 Create markdown.xml, project.default.xml, Enum.java, Interface.java, thymeleaf HTML File.html, _@user_Material Solarized Light.icls, GSP.xml, Default.xml, AngularJS .xml, file.template.settings.xml, find.xml, textmate_os.xml, keymap.xml, JSP.xml, _@user_Material Oceanic.icls, _@user_Darcula.icls, Zen HTML.xml, Relax Your Eyes.icls, mavenVersion.xml, freemarker.ftl, Default.xml, Eclipse copy.xml, Kotlin.xml, _@user_Arc Dark.icls, Zen XSL.xml, myTemplates.xml, BlueHippo.icls, iterations.xml, editor.xml, debugger.xml, updates.xml, baseRefactoring.xml, ide.general.xml, git.xml, _@user_Default.icls, lombok-plugin.xml, ui.lnf.xml, filetypes.xml, projectView.xml, javaeeExternalResources.xml, plugin_ui.xml, JavaScript Testing.xml, colors.scheme.xml, Codding Horror.icls, html_xml.xml, notifications.xml, archetypeCatalogs.xml, databaseDrivers.xml, github.xml, templates.xml, laf.xml, Class.java, jdk.table.xml, path.macros.xml, HTTP Request.xml, vcs.xml, databaseSettings.xml, toStringTemplates.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,9 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+  * @author:   ${liuchonglei}
+  * @date:     ${DATE} ${TIME}
+  * @description: ${DESCRIPTION}
+  */
+public class ${NAME} {
+}
